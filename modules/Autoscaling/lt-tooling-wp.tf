1c3c8e78f80503df016f7a6f0abcefa2197501cc
ojoimmanuel <ojoimmanuel@gmail.com>
2024-01-28 20:28:37 +0100

    launch template duplicates corrected

--- modules/Autoscaling/lt-tooling-wp.tf
+++ modules/Autoscaling/lt-tooling-wp.tf
@@ -1,75 +1,75 @@
-# launch template for wordpress
+# # launch template for wordpress
 
-resource "aws_launch_template" "wordpress-launch-template" {
-  image_id               = var.ami-web
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = var.web-sg
+# resource "aws_launch_template" "wordpress-launch-template" {
+#   image_id               = var.ami-web
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = var.web-sg
 
-  iam_instance_profile {
-    name = var.instance_profile
-  }
+#   iam_instance_profile {
+#     name = var.instance_profile
+#   }
 
-  key_name = var.keypair
-
-
-  placement {
-    availability_zone = "random_shuffle.az_list.result"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-
-  tags = merge(
-    var.tags,
-    {
-      Name = "wordpress-launch-template"
-    },
-  )
-  }
-
-  user_data = filebase64("${path.module}/wordpress.sh")
-}
+#   key_name = var.keypair
 
 
-# launch template for toooling
-resource "aws_launch_template" "tooling-launch-template" {
-  image_id               = var.ami-web
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = var.web-sg
+#   placement {
+#     availability_zone = "random_shuffle.az_list.result"
+#   }
 
-  iam_instance_profile {
-    name = var.instance_profile
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-  key_name = var.keypair
+#   tag_specifications {
+#     resource_type = "instance"
 
+#   tags = merge(
+#     var.tags,
+#     {
+#       Name = "wordpress-launch-template"
+#     },
+#   )
+#   }
 
-  placement {
-    availability_zone = "random_shuffle.az_list.result"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag_specifications {
-    resource_type = "instance"
+#   user_data = filebase64("${path.module}/wordpress.sh")
+# }
 
 
- tags = merge(
-    var.tags,
-    {
-      Name = "tooling-launch-template"
-    },
-  )
-  }
+# # launch template for toooling
+# resource "aws_launch_template" "tooling-launch-template" {
+#   image_id               = var.ami-web
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = var.web-sg
 
-  user_data = filebase64("${path.module}/tooling.sh")
-}
+#   iam_instance_profile {
+#     name = var.instance_profile
+#   }
+
+#   key_name = var.keypair
+
+
+#   placement {
+#     availability_zone = "random_shuffle.az_list.result"
+#   }
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+
+#   tag_specifications {
+#     resource_type = "instance"
+
+
+#  tags = merge(
+#     var.tags,
+#     {
+#       Name = "tooling-launch-template"
+#     },
+#   )
+#   }
+
+#   user_data = filebase64("${path.module}/tooling.sh")
+# }
 
 
 
