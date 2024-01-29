52812a66489a932428ca6c1418df076af79aa026
ojoimmanuel <ojoimmanuel@gmail.com>
2024-01-29 07:38:30 +0100

    update

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,29 +1,29 @@
 output "ALB-sg" {
-  value = aws_security_group.ACS["ext-alb-sg"].id
+  value = aws_security_group.emy["ext-alb-sg"].id
 }
 
 
 output "IALB-sg" {
-  value = aws_security_group.ACS["int-alb-sg"].id
+  value = aws_security_group.emy["int-alb-sg"].id
 }
 
 
 output "bastion-sg" {
-  value = aws_security_group.ACS["bastion-sg"].id
+  value = aws_security_group.emy["bastion-sg"].id
 }
 
 
 output "nginx-sg" {
-  value = aws_security_group.ACS["nginx-sg"].id
+  value = aws_security_group.emy["nginx-sg"].id
 }
 
 
 output "web-sg" {
-  value = aws_security_group.ACS["webserver-sg"].id
+  value = aws_security_group.emy["webserver-sg"].id
 }
 
 
 output "datalayer-sg" {
-  value = aws_security_group.ACS["datalayer-sg"].id
+  value = aws_security_group.emy["datalayer-sg"].id
 }
 
